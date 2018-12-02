d0c03ce651e295213746e1724964404160713bf2
neogeoss <neogeo-s@hanmail.net>
2018-12-03 00:35:10 +0900

    add free remove all nodes

--- dynlinkedlist/dynlist.c
+++ dynlinkedlist/dynlist.c
@@ -9,6 +9,37 @@ typedef struct {
 
 Node *head;
 
+void addFront(Node * root, int data) {
+	Node *node = (Node*)malloc(sizeof(Node));
+	node->data = data;
+	node->next = root->next;
+	root->next = node;
+}
+
+void removeFront(Node *root) {
+	Node * front = root->next;
+	root->next = front->next;
+	free(front);
+}
+
+void freeAll(Node * root) {
+	Node *cur = head->next;
+	while (cur != NULL) {
+		Node *next = cur->next;
+		free(cur);
+		cur = next;
+	}
+}
+
+void showAll(Node * root) {
+	Node *cur = head->next;
+	while (cur != NULL) {
+		printf("%d ", cur->data);
+		cur = cur->data;
+
+	}
+}
+
 int main(void) {
 
 	head = (Node*)malloc(sizeof(Node));
