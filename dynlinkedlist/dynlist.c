6b6d49b7b185aa71162b20d3745d5a934375392d
neogeoss <neogeo-s@hanmail.net>
2018-12-03 00:43:52 +0900

    complete linked list

--- dynlinkedlist/dynlist.c
+++ dynlinkedlist/dynlist.c
@@ -35,7 +35,7 @@ void showAll(Node * root) {
 	Node *cur = head->next;
 	while (cur != NULL) {
 		printf("%d ", cur->data);
-		cur = cur->data;
+		cur = cur->next;
 
 	}
 }
@@ -43,18 +43,15 @@ void showAll(Node * root) {
 int main(void) {
 
 	head = (Node*)malloc(sizeof(Node));
-	Node *node1 = (Node*)malloc(sizeof(Node));
-	node1->data = 1;
-	Node *node2 = (Node*)malloc(sizeof(Node));
-	node2->data = 2;
-	head->next = node1;
-	node1->next = node2;
-	node2->next = NULL;
-	Node *cur = head->next;
-	while (cur != NULL) {
-		printf("%d ", cur->data);
-		cur = cur->next;
-	}
+	head->next=NULL;
+	addFront(head, 2);
+	addFront(head, 1);
+	addFront(head, 7);
+	addFront(head, 9);
+	addFront(head, 8);
+	removeFront(head);
+	showAll(head);
+	freeAll(head);
 	system("pause");
 	return 0;
 }
